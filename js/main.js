1719a09e607fd3be7950979c7fbcf07c1623032f
FylSanya <34961973+FylSanya@users.noreply.github.com>
2020-07-10 18:32:16 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,5 @@
-// "use strict";
+"use strict";
 
-// let number = 5;
-// const leftBorderWidth = 1;
-
-// number = 10;
-// console.log(number);
-
-// const person = `Alexey`;
-// console.log(person);
-
-// let num = '11';
-// num += 1;
-// console.log(num);
-// num -= 1;
-// console.log(num);
-
-// const result = confirm("poshel k cherty");
 
 const numberOfFilms = +prompt("How much films are you already watch?", "");
 
