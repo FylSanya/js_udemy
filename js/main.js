fd4b343a3d504f489c600974b938884de329e77e
Alex Babaren <fffulls1zes@gmail.com>
2020-07-10 18:32:07 +0300

    first task

--- js/main.js
+++ js/main.js
@@ -1,19 +1,38 @@
-"use strict";
+// "use strict";
 
-let number = 5;
-const leftBorderWidth = 1;
+// let number = 5;
+// const leftBorderWidth = 1;
 
-number = 10;
-console.log(number);
+// number = 10;
+// console.log(number);
 
-const person = `Alexey`;
-console.log(person);
+// const person = `Alexey`;
+// console.log(person);
 
-let num = '11';
-num += 1;
-console.log(num);
-num -= 1;
-console.log(num);
+// let num = '11';
+// num += 1;
+// console.log(num);
+// num -= 1;
+// console.log(num);
 
-const result = confirm("poshel k cherty", "Da");
+// const result = confirm("poshel k cherty");
 
+const numberOfFilms = +prompt("How much films are you already watch?", "");
+
+let personaMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: "",
+    genres: [],
+    privat: false,
+};
+
+let name1 = prompt("Last film you've been watch?","");
+let rating1 = +prompt("How good is it?","");
+personaMovieDB.movies[name1] = rating1;
+
+let name2 = prompt("Last film you've been watch?","");
+let rating2 = +prompt("How good is it?"," ");
+personaMovieDB.movies[name2] = rating2;
+
+console.log(personaMovieDB);
