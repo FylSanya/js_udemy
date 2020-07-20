221b5471b990128560f55c69445158c27319d788
Alex Babaren <fffulls1zes@gmail.com>
2020-07-20 13:28:21 +0300

    3rd task

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 "use strict";
 
-let answer = 0;
+let numberOfFilms;
 
-const numberOfFilms = +prompt("How much films are you already watch?", "");
-console.log(numberOfFilms);
-
-
-
+function showMyDB(status) {
+    if(!status) {
+        console.log(personaMovieDB);
+    }
+}
 
 let personaMovieDB = {
     count: numberOfFilms,
@@ -17,26 +17,55 @@ let personaMovieDB = {
 };
 
 
-if(personaMovieDB.count < 10){
-    console.log("Ti loh");
-} else if (personaMovieDB.counts >= 10 && personaMovieDB.count <= 30){
-    console.log("Norm chel");
-} else if (personaMovieDB.count > 30) {
-    console.log("demon");
-} else {
-    console.log("hota ne to");
+
+function writeYourGenres(i) {
+    let answer = prompt(`Your ${i} favorite genre?`, "");
+    return answer;
 }
 
 
-for (let i = 0; i < 2; i++){
-    let name = prompt("Last film you've been watch?","");
-    let rating = +prompt("How good is it?","");
-    if(name == null || rating == null || name == "" || rating == "" || name.length > 50 || rating.length > 50) {
-        i--;
-    } else {
-    personaMovieDB.movies[name] = rating;
+function start() {
+    numberOfFilms = +prompt("How much films are you already watch?", "");
+
+    while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+        numberOfFilms = +prompt("How much films are you already watch?", "");
+    }
+
+    return numberOfFilms;
+}
+
+personaMovieDB.count = start();
+
+
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++){
+        let name = prompt("Last film you've been watch?","");
+        let rating = +prompt("How good is it?","");
+        if(name == null || rating == null || name == "" || rating == "" || name.length > 50 || rating.length > 50) {
+            i--;
+        } else {
+        personaMovieDB.movies[name] = rating;
+        }
     }
 }
 
+rememberMyFilms();
 
-console.log(personaMovieDB);
+for (let i = 0; i < 3; i++){
+    personaMovieDB.genres[i] = writeYourGenres(i+1);
+}
+function detectPersonaLevel(count) {
+    if(count < 10){
+        console.log("Ti loh");
+    } else if (count >= 10 && count <= 30){
+        console.log("Norm chel");
+    } else if (count > 30) {
+        console.log("demon");
+    } else {
+        console.log("shota ne to");
+    }
+}
+
+detectPersonaLevel(personaMovieDB.count);
+
+showMyDB(personaMovieDB.privat);
