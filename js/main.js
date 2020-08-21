03a27bc91bd2fac471fbdd3ed6c87f2748789ddc
Alex Babaren <fffulls1zes@gmail.com>
2020-08-21 20:06:00 +0300

    refactor

--- js/main.js
+++ js/main.js
@@ -2,70 +2,70 @@
 
 let numberOfFilms;
 
-function showMyDB(status) {
-    if(!status) {
-        console.log(personaMovieDB);
-    }
-}
-
 let personaMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: "",
     genres: [],
     privat: false,
-};
+    toggleVisibleMyDB: function () {
+        personaMovieDB.privat = !personaMovieDB.privat;
+    },
+    showMyDB: function (status) {
+        if(status == true) {
+            console.log(personaMovieDB);
+        }
+    },
+    writeYourGenres: function(i){
+        let answer;
+        while(answer == '' || answer == null) {
+            answer = prompt(`Your ${i} favorite genre?`, "");
+        }
+        return answer;
+    },
+    start: function(){
+        while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+            numberOfFilms = +prompt("How much films are you already watch?", "");
+        }
 
-
-
-function writeYourGenres(i) {
-    let answer = prompt(`Your ${i} favorite genre?`, "");
-    return answer;
-}
-
-
-function start() {
-    numberOfFilms = +prompt("How much films are you already watch?", "");
-
-    while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
-        numberOfFilms = +prompt("How much films are you already watch?", "");
-    }
-
-    return numberOfFilms;
-}
-
-personaMovieDB.count = start();
-
-
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++){
-        let name = prompt("Last film you've been watch?","");
-        let rating = +prompt("How good is it?","");
-        if(name == null || rating == null || name == "" || rating == "" || name.length > 50 || rating.length > 50) {
-            i--;
+        return numberOfFilms;
+    },
+    rememberMyFilms: function(){
+        for (let i = 0; i < 2; i++){
+            let name = prompt("Last film you've been watch?","");
+            let rating = +prompt("How good is it?","");
+            if(name == null || rating == null || name == "" || rating == "" || name.length > 50 || rating.length > 50 || isNaN(rating)) {
+                i--;
+            } else {
+            personaMovieDB.movies[name] = rating;
+            }
+        }
+    },
+    detectPersonaLevel: function(count){
+        if(count < 10){
+            console.log("Noob");
+        } else if (count >= 10 && count <= 30){
+            console.log("Typical guy");
+        } else if (count > 30) {
+            console.log("Cyberpis'ka");
         } else {
-        personaMovieDB.movies[name] = rating;
+            console.log("Something wrong");
         }
     }
-}
 
-rememberMyFilms();
+};
+
+personaMovieDB.count = personaMovieDB.start();
+personaMovieDB.rememberMyFilms();
+personaMovieDB.detectPersonaLevel(personaMovieDB.count);
+personaMovieDB.toggleVisibleMyDB();
+personaMovieDB.showMyDB(personaMovieDB.privat);
 
 for (let i = 0; i < 3; i++){
-    personaMovieDB.genres[i] = writeYourGenres(i+1);
+    personaMovieDB.genres[i] = personaMovieDB.writeYourGenres(i+1);
 }
-function detectPersonaLevel(count) {
-    if(count < 10){
-        console.log("Ti loh");
-    } else if (count >= 10 && count <= 30){
-        console.log("Norm chel");
-    } else if (count > 30) {
-        console.log("demon");
-    } else {
-        console.log("shota ne to");
-    }
-}
-
-detectPersonaLevel(personaMovieDB.count);
-
-showMyDB(personaMovieDB.privat);
+personaMovieDB.genres.forEach(element => {
+    let i = 0;
+    i++;
+    console.log(`Favorite genre #${personaMovieDB.genres.indexOf(element)+1} is - ${element}`);
+});
